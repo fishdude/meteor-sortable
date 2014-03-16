64439db399396fb6c0339e400d41a8f5a2dfe012
fishdude <d.n.fischer9@gmail.com>
2014-03-15 17:55:03 -0700

    v0.0.1

---
+++ package.js
@@ -0,0 +1,17 @@
+
+Package.describe({
+  summary: "Sortable, a lightwieght javascript library for reordering and sorting using the native HTML5 drag and drop API"
+});
+
+Package.on_use(function (api, where) {
+  
+
+  api.add_files('Sortable.min.js', 'client');
+
+});
+
+Package.on_test(function (api) {
+  api.use('meteor-sortable');
+
+  api.add_files('meteor-sortable_tests.js', ['client', 'server']);
+});
